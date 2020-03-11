c0134cb7f3a20c3a2451dd2a3cbb2a2861edfc26
bilalmac <bilalmac@bilalmac-iMac.local>
2020-03-11 16:25:57 -0700

    hellopickerview eklendi

---
+++ HelloPickerView/HelloPickerView/HelloPickerView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloPickerView
+//
+//  Created by bilalmac on 11.03.2020.
+//  Copyright © 2020 bilalmac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
